ff12d53a8bfb9aab8306792315f9b030930623dd
nashalysf <nashalysf@gmail.com>
2022-11-04 18:31:16 -0400

    install ft

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 
 module.exports = { 
-    mode: "development",
+    mode: "production",
     entry: "./src/js/index.js",
     output: {
         filename: 'bundle.js',
